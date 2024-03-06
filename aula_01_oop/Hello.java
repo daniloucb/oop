53a1c284422c2a48372c549d67c0fa398a9c1e60
daniloucb <danilo.brodrigues@a.ucb.br>
2024-03-05 21:22:26 -0300

    mudanças

--- aula_01_oop/Hello.java
+++ aula_01_oop/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       int age = 21;
+       double weight = 90.2;
+       float salary = 2.650f;
+       char gender = 'm';
+       boolean isActive = true;
+
+       System.out.println("idade: " + age);
+       System.out.println("peso: " + weight);
+       System.out.println("slário: " + salary);
+       System.out.println("gênero: " + gender);
+       System.out.println("está ativo?: " + isActive);
     }
 }
